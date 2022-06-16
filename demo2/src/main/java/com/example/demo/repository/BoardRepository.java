66b553b43c7da986e41c5bd11e5c93266dc4763c
고영빈 <45589210+skekq123@users.noreply.github.com>
2022-06-16 16:48:31 +0900

    repository, fetch join 사용

--- demo2/src/main/java/com/example/demo/repository/BoardRepository.java
+++ demo2/src/main/java/com/example/demo/repository/BoardRepository.java
@@ -2,8 +2,13 @@ package com.example.demo.repository;
 
 import com.example.demo.domain.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Integer> {
+    @Query("select distinct b from Board b left join fetch b.commentList")
+    List<Board> findAll();
 }
